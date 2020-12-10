dca4b0d4172641b4a5b2cd52f948c59875816dcc
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-12-10 11:32:46 -0500

    commit tree 4c95ba883315e8dc608b2e9a0713de47be997473

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    cleaned_str = self.delete(',')
+    string_arr = cleaned_str.split(/[[:punct:]]/)
+    string_arr.filter{ |elem| elem.length > 0 }.count
   end
+
 end
